61b92d11dfcac6b522bd77806e7f37468d09dee6
Kendall Robinson <kentechs91@gmail.com>
2023-08-13 14:38:14 -0400

    Added changes as directed in exercise.

---
+++ VariableTesting/src/Dog.java
@@ -0,0 +1,28 @@
+
+//public class Dog {
+	
+	//public static void main(String[] args) {
+	
+	//String name;
+	//String breed;
+	//int cost;
+	//String food = "Meat";
+	
+			//Dog d = new Dog();
+			
+			
+			
+			
+			
+			//System.out.println(d.name);
+			//System.out.println(d.breed);
+			//System.out.println(d.cost);
+			//System.out.println(d.answer);
+			//System.out.println(d.food);
+			
+			
+			
+			
+//		}
+
+//}
---
+++ VariableTesting/src/TestingVar.java
@@ -0,0 +1,30 @@
+
+public class TestingVar {
+	
+	public static void main(String[] args) {
+		
+		int num;
+		float b;
+		String aWord = "Hello";
+		String letter;
+		char c;
+		boolean answer;
+		double d;
+		//byte double;
+		
+		
+		
+		System.out.println(num);
+		System.out.println(b);
+		System.out.println(aWord);
+		//System.out.println(letter;
+		System.out.println(c);
+		System.out.println(answer);
+		System.out.println(d);
+		//System.out.println(byte);
+		
+		
+	}
+	
+
+}
